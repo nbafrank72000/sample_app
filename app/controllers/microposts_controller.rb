542d2f234685a49f51f8d686b998acf1a0df802b
frank <nbafrank72000@gmail.com>
2017-06-21 02:28:29 +0800

    Micropost with Image/picture

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -6,7 +6,7 @@ class MicropostsController < ApplicationController
 		@micropost = current_user.microposts.build(micropost_params)
 		if @micropost.save
 			flash[:success] = "Micropost created!"
-			redirect_to current_user
+			redirect_to root_url
 		else
 			feed_items = []
 			render 'static_pages/home'
@@ -22,7 +22,7 @@ class MicropostsController < ApplicationController
 	private
 
 	def micropost_params
-		params.require(:micropost).permit(:content)
+		params.require(:micropost).permit(:content, :picture)
 	end
 
 	def correct_user
